96e29a2fd8b97277ee41cb5f83df533dde0d786f
Sun Jiahao <sunjiahao231@mails.ucas.ac.cn>
2025-12-04 16:25:30 +0800

    initial gobang project

---
+++ config.h
@@ -0,0 +1,19 @@
+/*
+参数配置
+*/
+#ifndef CONFIG_H
+#define CONFIG_H
+#include <stdio.h>
+
+#define BOARD_SIZE 15
+
+#define EMPTY 0
+#define BLACK 1
+#define WHITE 2
+
+#define AI_MAX_DEPTH 4
+
+#define SCORE_FIVE 10000
+#define SCORE_LIVE_FOUR 9999
+
+#endif

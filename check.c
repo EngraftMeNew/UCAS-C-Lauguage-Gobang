6e41718e7f49eb13eb449d98cd130175e918ad35
Sun Jiahao <sunjiahao231@mails.ucas.ac.cn>
2025-12-09 23:36:47 +0800

    count_continuous

--- check.c
+++ check.c
@@ -1,3 +1,53 @@
 /*
 棋形/禁手检查
 */
+#include "config.h"
+#include "head.h"
+#include <stdio.h>
+
+int is_in_board(int row, int col)
+{
+    return (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE);
+}
+int is_legal_move(int row, int col, int color)
+{
+    if (!is_in_board(row, col))
+        return 0;
+    if (board[row][col] != 0)
+        return 0;
+    // TODO:加入黑棋的禁手判断
+
+    return 1;
+}
+
+int count_continuous(int row, int col, int dx, int dy, int color)
+{
+    int r = row;
+    int c = col;
+    int cnt = 0;
+    while (is_in_board(r, c) && board[r][c] == color)
+    {
+        cnt++;
+        r += dy;
+        c += dx;
+    }
+    r = row - dy;
+    c = col - dx;
+    while (is_in_board(r, c) && board[r][c] == color)
+    {
+        cnt++;
+        r -= dy;
+        c -= dx;
+    }
+}
+
+int five_check(int row, int col, int color)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        int len = count_continuous(row, col, dirs[i][0], dirs[i][1], color);
+        if (len >= 5)
+            return 1;
+    }
+    return 0;
+}
--- head.h
+++ head.h
@@ -6,7 +6,32 @@
 
 #include "config.h"
 extern int board[BOARD_SIZE][BOARD_SIZE];
-void init_board();
-void display_board();
-void input_move();
+
+const int dirs[4][2] = { //{dx,dy}
+    {1, 0},
+    {0, 1},
+    {1, 1},
+    {1, -1}};
+/*attention: dx 对应col dy对应row*/
+
+void init_board(void);
+void display_board(void);
+void input_move(void);
+int is_in_board(int row, int col);
+int is_legal_move(int row, int col, int color);
+int count_continuous(int row, int col, int dx, int dy, int color); // 某一方向上连续棋子
+int five_check(int row, int col, int color);
+int overline(int row, int col, int color);
+
+typedef struct
+{
+    int live_four;
+    int rush_four;
+    int live_three;
+    int sleep_three;
+} pattern;
+
+void analyze_line(int row, int col, int color, int dx, int dy, pattern *out); // 单独分析某个方向
+void analyze_point_patterns(int row, int col, int color, pattern *out);       // 某个点
+
 #endif
